96f50686df7b255a66f775a948cac80e0538caff
KashmirianNoodle <xackfaizu@gmail.com>
2025-03-07 14:55:20 +0530

    fix: minor fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { checkLocation } = require("../middlewares/search.middleware");
-const searchRoutes = require("./search");
+const searchRoutes = require("./search.route");
 
 router.use("/search", checkLocation, searchRoutes);
 
